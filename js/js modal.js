a4a0ae16878d0312fbe01ca39a380372f2d6d4fe
VasilBorodin <79115303+VasilBorodin@users.noreply.github.com>
2021-07-06 22:19:43 +0300

    Clean

---
+++ js/js modal.js
@@ -0,0 +1,6 @@
+
+const categoriesListItemRef = categoriesListRef.querySelectorAll('.item');
+categoriesListItemRef.forEach(category => {
+  console.log('Категория:', category.querySelector('h2').textContent);
+  console.log('Количество элементов:', category.querySelectorAll('li').length);
+});
