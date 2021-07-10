bd990e8525da907a1ce605322af996094b948333
VasilBorodin <79115303+VasilBorodin@users.noreply.github.com>
2021-07-10 11:17:48 +0300

    Переделал

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,5 +1,7 @@
-const categoriesListItemRef = categoriesListRef.querySelectorAll(".item");
-categoriesListItemRef.forEach((category) => {
+const categorsEl = document.querySelectorAll(".item");
+const categorAmount = categorsEl.length;
+console.log(`В списке ${categorAmount} категории.`);
+categorsEl.forEach((category) => {
   console.log("Категория:", category.querySelector("h2").textContent);
   console.log("Количество элементов:", category.querySelectorAll("li").length);
 });
