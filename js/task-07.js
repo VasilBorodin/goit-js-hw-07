bfbc0f89dd35cddc17ff0b4ac601cf69a4c06705
VasilBorodin <79115303+VasilBorodin@users.noreply.github.com>
2021-07-11 10:11:04 +0300

    Исправил 7

--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,6 @@ const inputRangeEl = document.querySelector("#font-size-control");
 const textEl = document.querySelector("#text");
 
 const onInputRateChange = (event) => {
-  textEl.getElementsByClassName.fontSize = event.target.value * 0.32 + "px";
+  textEl.style.fontSize = event.target.value * 0.32 + "px";
 };
 inputRangeEl.addEventListener("input", onInputRateChange);
